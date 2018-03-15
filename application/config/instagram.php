7c2ac27f7ba96169af51a73094e84a1a6b435a95
Julio Márquez <julesmqz@gmail.com>
2018-03-15 10:13:53 -0600

    config for instagram

---
+++ application/config/instagram.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$config['client_id'] = 'CLIENTID';
+$config['client_secret'] = 'CLIENTSECRET';
+$config['grant_type'] = 'authorization_code';
+$config['redirect_uri'] = 'http://jh.localhost/instalog.php';
+$config['access_token'] = NULL;
