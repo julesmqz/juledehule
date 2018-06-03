b5c3b6e0ad29a0e85d8fd967183cade913bdabda
Julio Márquez <julesmqz@gmail.com>
2018-06-03 14:43:38 -0500

    removing from repo

--- assets/js/global.js
+++
@@ -1,7 +0,0 @@
-// Secuencial
-$(document).ready(function(){
-    $(".instagram-pic").each(function(){
-        // Set same height as width in an effort to make this pic square
-        $(this).css('height',Math.round($(this).width()));
-    });
-});
--- assets/js/mainSlider.js
+++
@@ -1,6 +0,0 @@
-$('.main-slider').slick({
-    autoplay:true,
-    autoplaySpeed:3500,
-    prevArrow: '<button type="button" class="btn btn-outline-light slick-arrow slick-prev"> <i class="fa fa-angle-left" aria-hidden="true"></i> </button>',
-    nextArrow: '<button type="button" class="btn btn-outline-light slick-arrow slick-next"> <i class="fa fa-angle-right" aria-hidden="true"></i> </button>',
-});
